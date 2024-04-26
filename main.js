d02af3799d1855cc4fb7160cfe26751ad3c3e1ac
abdullohnajmiddinov <abdullohnajmiddinov45@gmail.com>
2024-04-26 18:29:44 +0300

    done

---
+++ main.js
@@ -0,0 +1,9 @@
+let pr1 =prompt('Ismingizni kiriting')
+let pr2 =+prompt('Space idingizni kiriting')
+let pr3 =+prompt('Space passwordingizni kiriting')
+let obj ={
+  name:pr1,
+  spaceId:pr2,
+  spacePassword:pr3,
+}
+console.log(obj);
